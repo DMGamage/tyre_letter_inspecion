9635435aed2445e7cac218d884473f68a33d204b
DMGamage <gamagemadhushan@gmail.com>
2023-07-02 11:29:02 +0530

    Histrogram equalization Part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 image = cv2.imread('02.png')
@@ -9,9 +11,15 @@ height = 800
 
 resized_image = cv2.resize(image, (width, height))
 
+gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+equalized = clahe.apply(gray)
+
+
 # Display the original and resized images
-cv2.imshow('Original Image', image)
-cv2.imshow('Resized Image', resized_image)
+cv2.imshow('Original Image', resized_image)
+cv2.imshow('Resized equalized', equalized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
